bfe7126be68d664c97e7849bdeb5ecaaa25f9dde
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-06-22 17:32:07 +0300

    Добавляются строки для подключения к ДБ

---
+++ internal/server/database/strings.go
@@ -0,0 +1,8 @@
+package database
+
+var dbDriver = "DB_DRIVER"
+var dbHost = "DB_HOST"
+var dbPort = "DB_PORT"
+var dbName = "POSTGRES_DB"
+var dbUser = "POSTGRES_USER"
+var dbPassword = "POSTGRES_PASSWORD"
