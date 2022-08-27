f62fd50ae3b60c926e1c744b600277db005546b9
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-08-27 22:36:27 +0300

    Создаются запросы для работы с таблицами токенов

--- cmd/internal/server/adapters/db/postgresql/queries/token.go
+++ cmd/internal/server/adapters/db/postgresql/queries/token.go
@@ -1,9 +1,9 @@
 package queries
 
 const (
-	QuerySelectTokenByUserId       string = `SELECT * FROM tokens WHERE userId=?;`
-	QuerySelectTokenByRefreshToken string = `SELECT * FROM tokens WHERE token=?;`
-	QueryUpdateToken               string = `UPDATE tokens SET token=? WHERE Id=?;`
-	QueryInsertToken               string = `INSERT INTO tokens (userId, token) VALUES (?, ?);`
-	QueryDeleteToken               string = `DELETE FROM tokens WHERE token=?;`
+	QuerySelectTokenByUserId       string = `SELECT * FROM tokens WHERE user_id=$1;`
+	QuerySelectTokenByRefreshToken string = `SELECT * FROM tokens WHERE token=$1;`
+	QueryUpdateToken               string = `UPDATE tokens SET token=$1 WHERE id=$2;`
+	QueryInsertToken               string = `INSERT INTO tokens (user_id, token) VALUES ($1, $2) RETURNING id;`
+	QueryDeleteToken               string = `DELETE FROM tokens WHERE token=$1;`
 )
