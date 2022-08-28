20589fe05951bb0019d37b2b9da893a9b56094f9
ShiryaevNikolay <shiryaev@surfstudio.ru>
2022-08-28 14:01:02 +0300

    Добавляются кавычки

--- cmd/internal/server/models/dto/tokens.go
+++ cmd/internal/server/models/dto/tokens.go
@@ -2,6 +2,6 @@ package dto
 
 // Сожержит access и refresh токены
 type Tokens struct {
-	Access  string `json:asscess_token`
-	Refresh string `json:refresh_token`
+	Access  string `json:"asscess_token"`
+	Refresh string `json:"refresh_token"`
 }
